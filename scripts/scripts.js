27d42750681358f4036967843a513c80806f199a
linahuchiyama <lmu12@bu.edu>
2023-12-04 22:04:53 -0500

    inital commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,14 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
+// const moodChoice = 
+// document.getElementById('moodChoice').addEventListener('click',function (e)) {
+//                                                        var img = document.createElement('img');
+// img.setAttribute('src','https://cdn.pixabay.com/photo/2022/02/21/15/10/tea-7026981_1280.png');
+
+
+// e.target.appendChild(img)
+
+function toggleCalm() {
+    document.getElementById("calmimg").style.visibility = document.getElementById("calmimg").style.visibility === "hidden" ? "visible" : "hidden";
+    }
+    
+                                                          
